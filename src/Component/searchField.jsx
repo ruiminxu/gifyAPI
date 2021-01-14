01573581aef66ec2cf74f81c07337252bbf6dd30
Haitham Alnajar <Haitham.alnajar98@gmail.com>
2021-01-14 14:09:51 -0500

    no changes

--- src/Component/searchField.jsx
+++ src/Component/searchField.jsx
@@ -1,52 +1,45 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 import GifCard from "./GifCard";
 
-export default class SearchField extends Component{
-    
-    constructor(props)
-    {
-        super(props);
-        this.state = {
-            searchTerm: '',
-            isClicked: false,
-        }
-    }
-    
-    handleSearch = (event) =>{
-        this.setState({
-            [event.target.name]: event.target.value
-        })
-    }
+export default class SearchField extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchTerm: "",
+      isClicked: false,
+    };
+  }
 
-    handleSubmit = (event) =>{
-        event.preventdefault()
-    }
+  handleSearch = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  };
 
-    handleSearchInput = () =>{
-        // console.log(this.state.searchTerm)
-        this.props.searchInput(this.state.searchTerm);
-    }
-    
-    render(){
-        return(
-            <div className = "searchBoxContainer">
-                <form onSubmit = {this.handleSubmit}>
-                    <input placeholder = "look up a gif" name = "searchTerm" onChange={this.handleSearch}></input>
-                </form>
-                <button onClick = {this.handleSearchInput}>Search</button>
-                {
-                    this.props.gif.map((gif) =>{
-                        
-                        return(
-                        <GifCard name = {gif.id} source = {gif.images.original.url}/>
-                        );
-                    })
-                }
-            </div>
-        );
-    }
+  handleSubmit = (event) => {
+    event.preventdefault();
+  };
+
+  handleSearchInput = () => {
+    // console.log(this.state.searchTerm)
+    this.props.searchInput(this.state.searchTerm);
+  };
+
+  render() {
+    return (
+      <div className="searchBoxContainer searchBox">
+        <form onSubmit={this.handleSubmit}>
+          <input
+            placeholder="look up a gif"
+            name="searchTerm"
+            onChange={this.handleSearch}
+          ></input>
+        </form>
+        <button onClick={this.handleSearchInput}>Search</button>
+        {this.props.gif.map((gif) => {
+          return <GifCard name={gif.id} source={gif.images.original.url} />;
+        })}
+      </div>
+    );
+  }
 }
-
-
-
-
